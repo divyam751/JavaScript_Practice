b51d0575559f275d0be4cd6665676e1108b8c2a7
Divyam Chauhan <mi.divyam@gmail.com>
2023-08-11 11:56:12 +0530

    Set1 completed

---
+++ Set1.js
@@ -0,0 +1,43 @@
+// Question 1
+
+let str = "This is string";
+let x = 20;
+
+console.log(str + x);
+
+// Question 2
+
+console.log(typeof (str + x));
+
+// Question 3
+
+const player = {
+  name: "Rohit Sharma",
+  maxScore: 200,
+  total_hundreds: 28,
+};
+
+// player = 232; // we can't do it
+// console.log(player);
+
+// Question 4
+
+player.total_fifty = 49;
+
+console.log(player);
+
+// Question 5
+
+const dictionary = {
+  mixtape:
+    "a recording on a cassette tape, CD, or digital medium, consisting of music or songs selected by a single person.",
+  demur:
+    "to make objection, especially on the grounds of moral or ethical considerations; take exception.",
+  cupidity:
+    "eager or excessive desire, especially to possess something; greed.",
+  ikigai:
+    "one’s reason for being, which in principle is the convergence of one’s personal passions, beliefs, values, and vocation.",
+  clout: "pull; strong influence.",
+};
+
+console.log(dictionary.clout);
