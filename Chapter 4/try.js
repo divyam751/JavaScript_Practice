98424ed296ba7c0bc7527c4a31361d073f9406d6
Divyam Chauhan <mi.divyam@gmail.com>
2023-08-13 08:51:05 +0530

    Chapter 4

---
+++ Chapter 4/try.js
@@ -0,0 +1,27 @@
+// let str = `He said you can't print "\`" inside backticks `;
+
+// console.log(str);
+
+let player1 = "Rahul";
+let player2 = "Sachin";
+
+// console.log(`player1 said "Happy Birthday" to player2`);
+// console.log(`${player1} said "Happy Birthday" to ${player2}`);
+
+// Q1
+
+console.log('har"'.length);
+
+// Q2
+const str = "RAHUL";
+
+console.log(str.startsWith("r"));
+console.log(str.endsWith("L"));
+
+// Q3
+
+let str1 = "HI I AM Rahul";
+
+console.log(str1.toLowerCase());
+
+// Q4
