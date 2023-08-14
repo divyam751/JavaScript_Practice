65461daf76e916d4c27f5ee9b1d0ece3eece5f62
Divyam Chauhan <mi.divyam@gmail.com>
2023-08-14 20:36:33 +0530

    Chapter 6

---
+++ Chapter 6/try.js
@@ -0,0 +1,46 @@
+// // Q1
+
+// let age = prompt("Enter your Age");
+// age = Number.parseInt(age);
+// let res = age<18? "not drive":"drive";
+// document.write(`You can ${res}`);
+
+// // Q2
+// let age = prompt("Enter your Age");
+
+// let resubmit = confirm("Do you want to see the propmt again?")
+// if(resubmit){
+//   age = prompt("Enter your Age");
+// }
+
+// age = Number.parseInt(age);
+// let res = age<18? "not drive":"drive";
+// document.write(`You can ${res}`);
+
+// // Q3
+// let age = prompt("Enter your Age");
+
+// let resubmit = confirm("Do you want to see the propmt again?")
+// if(resubmit){
+//   age = prompt("Enter your Age");
+// }
+
+// if (age<0){
+//   console.error("Entered Age is invalid");
+// }
+// age = Number.parseInt(age);
+// let res = age<18? "not drive":"drive";
+// document.write(`You can ${res}`);
+
+// // Q4
+// let age = prompt("Enter your Age");
+// age = Number.parseInt(age);
+
+// if(age>4){
+//   window.open("https://google.com");
+// }
+
+// Q5
+let color = prompt("Enter page  color");
+
+document.body.style.background = color;
--- README.md
+++ README.md
@@ -43,3 +43,46 @@ for (let i in num) {
   //   that will print only keys like  0,1,2,3,4,5
 }
 ```
+
+### These functions will not alter original array
+
+# Map
+
+```javascript
+let arr = [1, 4, 3, 6, 8, 4];
+
+// Map will return a new array
+
+let new_arr = arr.map((ele, index) => {
+  return ele + index;
+});
+
+console.log(new_arr);
+```
+
+# Filter
+
+```javascript
+//  It will also return a new array;
+let arr = [1, 4, 3, 6, 8, 4];
+
+let elements = arr.filter((ele) => {
+  return ele < 5;
+});
+
+console.log(elements);
+```
+
+# Reduce
+
+```javscript
+
+
+let arr = [1, 3, 4, 5, 3];
+
+let sum = arr.reduce((var1, var2) => {
+  return var1 + var2;
+});
+
+console.log(sum);
+```
